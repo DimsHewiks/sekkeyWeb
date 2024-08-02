f80c03039815624541460b3be13facf3e3a9d878
DimsHewiks <alex.gorsia.com@mail.ru>
2024-08-02 23:14:43 +0800

    обновление роута

--- src/routes/authPage/UI/authBlock/AuthBlock.jsx
+++ src/routes/authPage/UI/authBlock/AuthBlock.jsx
@@ -1,9 +1,26 @@
+import { useRef } from 'react'
 import Style from './authBlock.module.css'
 
 export default function AuthBlock(){
+
+
+
+    let mainBlockView = useRef('');
+
+    function getStartRegist(){
+        mainBlockView.current.style.opacity = 0;
+        console.log(mainBlockView)
+        setTimeout(()=>{
+            window.location.href = '/'
+        }, .2)
+    }
+
+
+
+
     return(
         <>
-        <div className={Style.mainBlock}>
+        <div ref ={mainBlockView} className={Style.mainBlock} style={{opacity: 1}}>
            <div className={Style.FormBlock}>
                 <h2 className={Style.MainTitle}>Авторизация</h2>
                 <form className={Style.mainForm}>
@@ -19,15 +36,15 @@ export default function AuthBlock(){
                             <input className={Style.InputStyle} placeholder="Пароль..." type='text'></input>
                         </div>
                     </div>
-                    <div>
-                        <div>
+                    <div className={Style.BtnBlock}>
+                        <div className={Style.SingInBtn}>
                             <p>ВОЙТИ</p>
                         </div>
-                        <div>
+                        <div className={Style.SupportBtn}>
                             <p>Забыл пароль</p>
                         </div>
                     </div>
-                    <div>
+                    <div className={Style.SingUpBtn} onClick={()=>getStartRegist()}>
                         <p>
                             Пройти регистрацию
                         </p>
