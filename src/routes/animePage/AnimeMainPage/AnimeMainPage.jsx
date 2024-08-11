957aa49143c0ad3e71d616a318607450a99bf762
DimsHewiks <alex.gorsia.com@mail.ru>
2024-08-11 19:52:15 +0800

    добавил страницу с аниме

---
+++ src/routes/animePage/AnimeMainPage/AnimeMainPage.jsx
@@ -0,0 +1,12 @@
+import TopAnimeHeader from "./elements/TopAnimeHeader/TopAnimeHeader";
+
+export default function AnimeMainPage(){
+    return(
+        <>
+        <h1>
+            Аниме
+        </h1>
+        <TopAnimeHeader />
+        </>
+    )
+}
